c8a4c936ae1477745f977fa0e75f8d6a0f1161cd
Ryan Choi <ryan.choi@webadvanced.com>
2017-11-30 15:16:42 -0800

    Functionalities

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 Vue.component('tasks-list', {
   props: ['list'],
-  template: '#tasks-template'
+  template: '#tasks-template',
+  computed: {
+    remaining: function() {
+      return this.list.filter(this.isInProgress).length;
+    }
+  },
+  methods: {
+    isCompleted: function(task) {
+      return task.completed;
+    },
+    isInProgress: function(task) {
+      return ! this.isCompleted(task);
+    },
+    deleteTask: function(key) {
+      Vue.delete(this.list, key);
+    },
+    clearCompleted: function() {
+      this.list = this.list.filter(this.isInProgress);
+    }
+  }
 });
 
 new Vue({
