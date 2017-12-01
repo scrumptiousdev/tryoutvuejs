fa2093434b41da16754da6ee7949a66f2fe9e4ea
Ryan Choi <ryan.choi@webadvanced.com>
2017-11-30 16:37:08 -0800

    Bringing back functionalities with ajax data from local json file

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 Vue.component('tasks-list', {
   template: '#tasks-template',
+  computed: {
+    remaining: function() {
+      return this.list.filter(this.isProgress).length;
+    }
+  },
   data: function() {
     return {
       list: []
@@ -8,15 +13,24 @@ Vue.component('tasks-list', {
   created: function() {
     this.fetchTaskList();
   },
-  methods: function() {
+  methods: {
     fetchTaskList: function() {
       var vm = this;
-      $.getJSON('api-address-here', function(tasks) {
-        vm.list = tasks;
+      $.getJSON('data.json', function(tasks) {
+        vm.list = tasks.data;
       });
-    }
+    },
     deleteTask: function(key) {
       Vue.delete(this.list, key);
+    },
+    isCompleted: function(task) {
+      return task.completed;
+    },
+    isProgress: function(task) {
+      return ! this.isCompleted(task);
+    },
+    clearCompleted: function() {
+      this.list = this.list.filter(this.isProgress);
     }
   }
 });
