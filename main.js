456234a6639ff6b1b2a45c2563f0e15b93ae8157
Ryan Choi <ryan.choi@webadvanced.com>
2017-12-01 15:08:35 -0800

    Vue transitions and animations

--- main.js
+++ main.js
@@ -1,32 +1,6 @@
-var bus = new Vue();
-
-Vue.component('message', {
-  template: '<input v-model="message" @keyup.enter="storeMessage">',
-  data: function() {
-    return { message: '' };
-  },
-  methods: {
-    storeMessage: function() {
-      bus.$emit('new-message', this.message);
-      this.message = '';
-    }
-  }
-});
-
 new Vue({
   el: '#app',
   data: {
-    messages: []
-  },
-  methods: {
-    handleMessage: function(message) {
-      this.messages.push(message);
-    }
-  },
-  created: function() {
-    bus.$on('new-message', this.handleMessage);
-  },
-  destroyed: function() {
-    bus.$off('new-message', this.handleMessage);
+    show: true
   }
 });
