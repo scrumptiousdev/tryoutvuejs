b86c613e7af349868deabb52af94e5349122c99c
Ryan Choi <ryan.choi@webadvanced.com>
2017-12-01 16:35:38 -0800

    Component within component

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+Vue.component('task-list', {
+  template: '<div><task v-for="task in tasks">{{ task.task }}</task></div>',
+  data: function() {
+    return {
+      tasks: [
+        { task: 'Go to the bank', complete: true },
+        { task: 'Go to the store', complete: false },
+        { task: 'Go to the farm', complete: true },
+        { task: 'Go to work', complete: false }
+      ]
+    }
+  }
+});
+
 Vue.component('task', {
   template: '<li><slot></slot></li>'
 });
