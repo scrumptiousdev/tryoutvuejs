837b9fd683d1ddc28f26bb9735b4a1f78ec54933
Ryan Choi <ryan.choi@webadvanced.com>
2017-12-03 13:45:04 -0800

    Multiple named slots with default option

--- main.js
+++ main.js
@@ -1,39 +1,27 @@
-// function Event() {
-//   this.vue = new Vue();
-// }
-//
-// Event.prototype.fire = function(event, data) {
-//   this.vue.$emit(event, data);
-// };
-//
-// Event.prototype.listen = function(event, callback) {
-//   this.vue.$on(event, callback);
-// };
-//
-// window.Event = new Event();
-
-// OR
-
-window.Event = new Vue();
-
-Vue.component('coupon', {
-  template: '<input placeholder="Enter your coupon code" @blur="onCouponApplied">',
-  methods: {
-    onCouponApplied: function() {
-      Event.$emit('applied');
-    }
-  }
+Vue.component('modal', {
+  template: '<div class="modal is-active">\
+      <div class="modal-background"></div>\
+      <div class="modal-card">\
+        <header class="modal-card-head">\
+          <p class="modal-card-title">\
+            <slot name="header"></slot>\
+          </p>\
+          <button class="delete" aria-label="close"></button>\
+        </header>\
+        <section class="modal-card-body">\
+          <slot>\
+            Default content here.\
+          </slot>\
+        </section>\
+        <footer class="modal-card-foot">\
+          <slot name="footer">\
+            <button class="button is-success">Save</button>\
+          </slot>\
+        </footer>\
+      </div>\
+    </div>',
 });
 
 new Vue({
   el: '#app',
-  data: {
-    couponApplied: false
-  },
-  created: function() {
-    $vm = this;
-    Event.$on('applied', function() {
-      $vm.couponApplied = true;
-    })
-  }
 });
